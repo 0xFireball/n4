02f3e582b4a2d4c9bb9384fa0d1579be53fa0233
0xFireball <0xFireball@outlook.com>
2017-02-07 17:43:26 -0800

    add render callbacks

--- Sources/n4/NGameFrame.hx
+++ Sources/n4/NGameFrame.hx
@@ -1,11 +1,13 @@
 package n4;
 
+import kha.Framebuffer;
+import kha.Scheduler;
 import kha.System;
 
 class NGameFrame {
 
-	private var _state:NState = null;
-	private var _targetFramerate:Int;
+	private static var _state:NState = null;
+	private static var _targetFramerate:Int;
 
 	public static function init(?Title:String = "n4", ?Width:Int = 800, ?Height:Int = 600, ?Framerate:Int = 60) {
 		_targetFramerate = Framerate;
@@ -18,16 +20,16 @@ class NGameFrame {
 		_state = state;
 	}
 
-	private static function ge_update() {
+	private static function ge_update():Void {
 
 	}
 
-	private static function ge_render() {
+	private static function ge_render(framebuffer: Framebuffer): Void {
 
 	}
 
 	private static function onInit() {
-		System.notifyOnRender(render);
-		Scheduler.addTimeTask(update, 0, 1 / 60);
+		System.notifyOnRender(ge_render);
+		Scheduler.addTimeTask(ge_update, 0, 1 / _targetFramerate);
 	}
 }
