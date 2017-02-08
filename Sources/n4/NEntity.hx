504310343d0941681081e51eff87e8cf9767e613
0xFireball <0xFireball@outlook.com>
2017-02-07 18:25:31 -0800

    add entity implementatino

--- Sources/n4/NEntity.hx
+++ Sources/n4/NEntity.hx
@@ -1,7 +1,74 @@
 package n4;
 
+import kha.Framebuffer;
+import kha.math.FastVector2;
+
 class NEntity extends NBasic {
-	public function new() {
+
+	/**
+	 * X position of the upper left corner of this object in world space.
+	 */
+	public var x(default, set):FastFloat = 0;
+	/**
+	 * Y position of the upper left corner of this object in world space.
+	 */
+	public var y(default, set):FastFloat = 0;
+	/**
+	 * The width of this entity's hitbox. For sprites, use offset to control the hitbox position.
+	 */
+	@:isVar
+	public var width(get, set):FastFloat;
+	/**
+	 * The height of this entity's hitbox. For sprites, use offset to control the hitbox position.
+	 */
+	@:isVar
+	public var height(get, set):FastFloat;
+
+	/**
+	 * The basic speed of this object (in pixels per second).
+	 */
+	public var velocity(default, null):FastVector2;
+	/**
+	 * How fast the speed of this object is changing (in pixels per second).
+	 * Useful for smooth movement and gravity.
+	 */
+	public var acceleration(default, null):FastVector2;
+	/**
+	 * This isn't drag exactly, more like deceleration that is only applied
+	 * when acceleration is not affecting the sprite.
+	 */
+	public var drag(default, null):FastVector2;
+	/**
+	 * If you are using acceleration, you can use maxVelocity with it
+	 * to cap the speed automatically (very useful!).
+	 */
+	public var maxVelocity(default, null):FastVector2;
+
+	/**
+	 * Utility for storing health
+	 */
+	public var health:Float = 1;
+
+	public function new(X:Float = 0, Y:Float = 0, Width:Float = 0, Height:Float = 0) {
 		super();
+
+		x = X;
+		y = Y;
+		width = Width;
+		height = Height;
+
+		init();
+	}
+
+	private function init() {
+
+	}
+
+	override public function update(dt:Float):Void {
+		super.update(dt);
+	}
+
+	override public function render(f:Framebuffer):Void {
+		super.render(f);
 	}
 }
